004c9c7019a80416467c09d74b87215aa999d11c
Stefan Dobrowolny <beaf-o@gmx.de>
2015-08-01 23:29:21 +0200

    clean images on startup, suffix for log files

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 dir="/home/pi/monitor"
+dateSuffix=`date +'%Y-%m-%d'`
 
 cd $dir
-python $dir/get_data.py 2>&1 | tee -a $dir/logs/get_data.log &
-python $dir/show_data.py 2>&1 | tee -a $dir/logs/show_data.log &
+
+rm -rf webcam/*
+rm -rf strikes/*
+rm -rf rain/*
+
+python $dir/get_data.py 2>&1 | tee -a $dir/logs/get_data_${dateSuffix}.log &
+python $dir/show_data.py 2>&1 | tee -a $dir/logs/show_data_${dateSuffix}.log &
